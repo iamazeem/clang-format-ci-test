eb0d158655b58646e6b79432efcfa7674ab17d69
Azeem Sajid <azeem.sajid@gmail.com>
2022-04-19 19:56:26 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,5 @@
-#include <iostream>
+#include"test.hpp"
 #include <cstdlib>
-
-// --- test class ---
-
-namespace test {
-
-    class Test final {
-    public:
-    Test(const std::string id) noexcept;
-    ~Test() noexcept;
-
-private:
-    std::string m_id{};
-};
-    
-Test::Test(const std::string id) noexcept
-:
-m_id{id}
-{
-    std::cout << m_id << ": created\n";
-}
-Test::~Test() noexcept { std::cout << m_id << ": destroyed\n"; }
-
-} // test
-
-// --- main ---
 
 int main() {
     test::Test t{"test-id"};
