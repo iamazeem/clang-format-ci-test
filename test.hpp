dcd5170ba6a6547fdc6a6f3bf4c523a9082bebe0
Azeem Sajid <azeem.sajid@gmail.com>
2022-04-19 19:57:13 +0500

    Create test.hpp

---
+++ test.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include<string>
+#include<iostream>
+
+// --- test class ---
+
+namespace test {
+
+    class Test final {
+    public:
+    Test(const std::string id) noexcept;
+    ~Test() noexcept;
+
+private:
+    std::string m_id{};
+};
+    
+Test::Test(const std::string id) noexcept
+:
+m_id{id}
+{
+    std::cout << m_id << ": created\n";
+}
+Test::~Test() noexcept { std::cout << m_id << ": destroyed\n"; }
+
+} // test
