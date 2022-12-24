95830471a87b5ddac279faf37f286ed82655975e
Azeem Sajid <azeem.sajid@gmail.com>
2022-12-24 12:02:50 +0500

    Update.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # clang-format-ci-test
+
+[![ci](https://github.com/iamazeem/clang-format-ci-test/actions/workflows/ci.yml/badge.svg)](https://github.com/iamazeem/clang-format-ci-test/actions/workflows/ci.yml)
+
 Automate clang-format via CI on PR/commit (main branch)
--- test.hpp
+++ test.hpp
@@ -5,26 +5,28 @@
 
 // --- test class ---
 
-namespace test
-{
+namespace test {
 
 class Test final
 {
 public:
-    Test(const std::string id) noexcept;
+    Test(const std::string& id) noexcept;
     ~Test() noexcept;
 
 private:
     std::string m_id{};
 };
 
-Test::Test(const std::string id) noexcept : m_id{id}
+Test::Test(const std::string& id) noexcept
+    :
+    m_id{id}
 {
     std::cout << m_id << ": created\n";
 }
+
 Test::~Test() noexcept
 {
     std::cout << m_id << ": destroyed\n";
 }
 
-} // namespace test
+}
